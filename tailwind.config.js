2011381682abaa0026e594cfd39080265d760762
Aris Chow <arischow@gmail.com>
2022-05-29 18:39:49 +0800

    site branding

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
-  content: ["./frontend/**/*.{html,js}"],
+  content: ['./frontend/**/*.{html,js}'],
   theme: {
     extend: {},
+    fontFamily: {
+      'title': ['Crimson Text', 'serif'],
+      'intro': ['Karla', 'sans-serif'],
+    }
   },
   plugins: [],
 }
