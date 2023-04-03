e8ee93f4ff14778edf67d7bcc702cfac79f151b6
Aris Chow <arischow@gmail.com>
2023-04-03 23:14:23 +0800

    Django project & app, refs #18

--- contents/pages/about.md
+++
@@ -1,7 +0,0 @@
----
-title: About Me
----
-
-I'm a software engineer. During my career, I worked in a DevOps role for about two years. You can find articles about programming here.
-
-Feel free to ping me on [Twitter](https://twitter.com/ManhoChow).
--- src/gomashio/config.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-
-PROJECT_ROOT_DIR = Path(__file__).resolve(strict=True).parent.parent.parent
-FRONTEND_DIR = Path.joinpath(PROJECT_ROOT_DIR, "frontend")
-TEMPLATE_DIR = Path.joinpath(FRONTEND_DIR, "templates")
-CONTENTS_DIR = Path.joinpath(PROJECT_ROOT_DIR, "contents")
-DIST_DIR = Path.joinpath(PROJECT_ROOT_DIR, "dist")
